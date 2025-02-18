629cbc737bf302aa8068cf90cda3d5fc43360b57
Abhijith12371 <abpokeunite@gmail.com>
2025-02-18 21:25:06 +0530

    Initial commit from Create Next App

---
+++ app/Components/InputContainer.jsx
@@ -0,0 +1,11 @@
+const InputContainer=()=>{
+    return(
+        <>
+        <input placeholder="upload the file here" type="file"/>
+        <button>Send</button>
+        </>
+    )
+}
+
+
+export default InputContainer;
